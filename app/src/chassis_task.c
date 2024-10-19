311f34fd6586f7b332dc6a812cee2444c210e239
Sanjith Cherumandanda <sanjithc4@outlook.com>
2024-10-19 14:22:05 -0400

    Defining chassis motor controllers

--- app/src/chassis_task.c
+++ app/src/chassis_task.c
@@ -2,18 +2,41 @@
 
 #include "robot.h"
 #include "remote.h"
+#include "dji_motor.h"
+#include "omni_kinematics.h"
+#define WHEEL_DIAMETER (0.07f)
 
 extern Robot_State_t g_robot_state;
 extern Remote_t g_remote;
 
-float chassis_rad;
+
+DJI_Motor_Handle_t *chassis_motors[4];
+
 
 void Chassis_Task_Init()
 {
-    // Init chassis hardware
+    
+    for (int i = 1; i < 5; i++)
+    {
+        Motor_Config_t chassis_motor_config = {
+        .can_bus = 1,
+        .speed_controller_id = i,
+        .offset = 0,
+        .control_mode = VELOCITY_CONTROL,
+        .motor_reversal = MOTOR_REVERSAL_NORMAL,
+        .velocity_pid = 
+            {
+                .kp = 1000.0f,
+                .ki = 0.0f,
+                .kd = 100.0f,
+                .output_limit = M2006_MAX_CURRENT,
+            }};
+        chassis_motors[i] = DJI_Motor_Init(&chassis_motor_config, M2006);
+    }
 }
 
 void Chassis_Ctrl_Loop()
 {
+    
     // Control loop for the chassis
 }
